8cfb51be5b4e1a7d47a88afc1cbe216a52bc8a6e
Suhail-Ka-XPS-UWindsor <bains.singh.suhail@gmail.com>
2021-10-16 00:05:40 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/ocr/TextManipulation.java
@@ -0,0 +1,53 @@
+package com.example.ocr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.TextView;
+
+public class TextManipulation extends AppCompatActivity {
+    private float defFontsize = 14f;
+    private String textData = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent i = getIntent();
+        textData = i.getStringExtra("testString");
+        textData = "textString";
+        setContentView(R.layout.activity_text_manipulation);
+        TextView textShow = (TextView) findViewById (R.id.textShow);
+        textShow.setText(textData);
+
+    }
+
+    public void ZoomOut (View view) {
+        defFontsize -= 4f;
+        if(defFontsize <= 4f)
+            defFontsize = 4f;
+        TextView textShow = (TextView) findViewById (R.id.textShow);
+        textShow.setTextSize(TypedValue.COMPLEX_UNIT_SP, defFontsize);
+        // Do something in response to button click
+    }
+
+    public void ZoomIn (View view) {
+        defFontsize += 4f;
+        TextView textShow = (TextView) findViewById (R.id.textShow);
+        textShow.setTextSize(TypedValue.COMPLEX_UNIT_SP, defFontsize);
+        // Do something in response to button click
+    }
+
+
+    public void Share(View view) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, textData);
+        sendIntent.setType("text/plain");
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
+    }
+}
